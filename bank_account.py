7d1ae3addea04907d507d031876c9a0e3cdffd8e
meganclapinski25 <142531799+meganclapinski25@users.noreply.github.com>
2024-10-24 12:03:22 -0700

    Added deposit method, and tests

--- bank_account.py
+++ bank_account.py
@@ -4,6 +4,22 @@ class BankAccount:
     def __init__(self, full_name):
         self.full_name = full_name
         self.balance = 0
-        self.account_number = random_num()
-    def random_num():
+        self.account_number = self.random_num()
+    def random_num(self):
         return random.randint(10000000,99999999)
+    
+    def deposit(self, amount):
+        self.balance += amount 
+        return ("Amount deposited: + ${amount} + new balance: + ${self.balance}" )
+    
+    
+account1 = BankAccount("Megan Clapinski")
+print(f"Account Owner: {account1.full_name}")
+print(f"Account Number: {account1.account_number}")
+print(f"Initial Balance: ${account1.balance}")
+
+
+account1.deposit(500)  
+
+
+print(f"Updated Balance: ${account1.balance}")
