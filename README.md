d076aed2277a4930b7bb0d1212c3bfe559ea0618
meganclapinski25 <142531799+meganclapinski25@users.noreply.github.com>
2024-10-30 10:05:20 -0700

    added blurred accont number and more readme

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Bank-Account
+
+## Summary
+
+### This Bank Account program stimulates the very thing you do at an ATM. You are able to depsit, withdraw, check your balance, and create an account. 
+
+
+# How to Use 
+
+## 1. Clone this repo to your local machine 
--- bank_account.py
+++ bank_account.py
@@ -1,33 +1,41 @@
 import random
 
+
+
 class BankAccount:
-    def __init__(self, full_name, account_number = None):
+    def __init__(self, full_name, account_number = None): #Global to class 
         self.full_name = full_name
         self.balance = 0
         self.account_number = account_number if account_number else self.random_num()
+    
+    #Account number using random + 8 digits
     def random_num(self):
         return random.randint(10000000,99999999)
     
+    # Deposit (balance = balance + amount)
     def deposit(self, amount):
         self.balance += amount 
         print (f"Amount deposited:  ${amount}  new balance:  ${self.balance:.2f}" )
-    
+    #Withdraw (balance = balance - withdraw)
     def withdraw(self, amount):
         self.balance -= amount
         print (f"Amount withdrew:  ${amount}  new balance:  ${self.balance:2f}" )
     
+    #Print Balance
     def get_balance(self):
         print(f"Your current balance is : ${self.balance:.2f}")
     
+    # Add Interest to balance (interest = balance * 8.3% )
     def add_interest(self):
         interest = self.balance * 0.0083
         self.balance -=interest
         print(f"Your current balanace with interest is : ${self.balance:.2f}")    
-        
+    # Print statment with blurred account number 
     def print_statment(self):
         print(f"Account Owner: {self.full_name}")
-        print(f"Account Number: {self.account_number}")
-        print(f" Balance: ${self.balance}")
+        masked_account_number = "****" + str(self.account_number)[-4:]
+        print(f"Account No: {masked_account_number}")
+        print(f"Balance: ${self.balance}")
         
         
         
